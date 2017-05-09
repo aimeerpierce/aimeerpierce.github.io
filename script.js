c5b079695da0c1a95f802b19c1fd591337d7f007
aimeerpierce <aimeerpierce@gmail.com>
2017-05-09 00:17:31 -0500

    added hover icons and right column

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+    var swap_img = function () {
+        var $this = $(this);
+        var newSource = $this.data('alt-src');
+        $this.data('alt-src', $this.attr('src'));
+        $this.attr('src', newSource);
+    }
 
 $('document').ready(function(){
 	$("a").hover(function(){
 		$(this).css("color","#878787");
 	}, function() {
 		$(this).css("color","#5f5f5f");
+	});
+
+	$(function () {
+    	$('img.github').hover(swap_img, swap_img);
+	});
+	$(function () {
+    	$('img.email').hover(swap_img, swap_img);
+	});
+	$(function () {
+    	$('img.facebook').hover(swap_img, swap_img);
+	});
+	$(function () {
+    	$('img.linkedin').hover(swap_img, swap_img);
+	});
+	$(function () {
+    	$('img.note').hover(swap_img, swap_img);
 	});
 });
