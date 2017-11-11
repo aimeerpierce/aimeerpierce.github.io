68951d571f1ad1e714668a3cf334c22a538399a6
aimeerpierce <aimeerpierce@gmail.com>
2017-11-10 18:26:34 -0600

    update 11/10: added background photo and updated navbar

---
+++ icons/Aimee's Website_files/script.js
@@ -0,0 +1,130 @@
+var swap_img = function () {
+    var $this = $(this);
+    var newSource = $this.data('alt-src');
+    $this.data('alt-src', $this.attr('src'));
+    $this.attr('src', newSource);
+}
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+$('document').ready(function(){
+	$("a").hover(function(){
+		$(this).css("color","#878787");
+	}, function() {
+		$(this).css("color","#5f5f5f");
+	});
+	$("li").hover(function(){
+		$(this).css("color","#878787");
+	}, function() {
+		$(this).css("color","#5f5f5f");
+	});
+
+	$(function () {
+    	$('img.github').hover(swap_img, swap_img);
+	});
+	$(function () {
+    	$('img.email').hover(swap_img, swap_img);
+	});
+	$(function () {
+    	$('img.facebook').hover(swap_img, swap_img);
+	});
+	$(function () {
+    	$('img.linkedin').hover(swap_img, swap_img);
+	});
+	$(function () {
+    	$('img.note').hover(swap_img, swap_img);
+	});
+});
+
+function toggle_aboutme() {
+	var aboutme = document.getElementById('aboutme');
+	var exp = document.getElementById('experience');
+	var working = document.getElementById('working');
+	var passions = document.getElementById('passions');
+	var val = window.getComputedStyle(aboutme).getPropertyValue("display");
+	if (val == 'none') {
+	    aboutme.style.display = 'block';
+	    exp.style.display = 'none';
+	    working.style.display = 'none';
+	    passions.style.display = 'none';
+	}
+}
+
+function toggle_exp() {
+	var aboutme = document.getElementById('aboutme');
+	var exp = document.getElementById('experience');
+	var working = document.getElementById('working');
+	var passions = document.getElementById('passions');
+	var val = window.getComputedStyle(exp).getPropertyValue("display");
+	if (val == 'none') {
+	    exp.style.display = 'block';
+	    aboutme.style.display = 'none';
+	    working.style.display = 'none';
+	    passions.style.display = 'none';
+
+	}
+}
+
+function toggle_working() {
+	var aboutme = document.getElementById('aboutme');
+	var exp = document.getElementById('experience');
+	var working = document.getElementById('working');
+	var passions = document.getElementById('passions');
+
+	var val = window.getComputedStyle(working).getPropertyValue("display");
+	if (val == 'none') {
+	    working.style.display = 'block';
+	    exp.style.display = 'none';
+	    aboutme.style.display = 'none';
+	    passions.style.display = 'none';
+
+	}
+}
+function toggle_passions() {
+	var aboutme = document.getElementById('aboutme');
+	var exp = document.getElementById('experience');
+	var working = document.getElementById('working');
+	var passions = document.getElementById('passions');
+
+	var val = window.getComputedStyle(passions).getPropertyValue("display");
+	if (val == 'none') {
+	    passions.style.display = 'block';
+	    exp.style.display = 'none';
+	    aboutme.style.display = 'none';
+	    working.style.display = 'none';
+
+	}
+}
+
+function open_resume(){
+	var win = window.open("https://docs.google.com/document/d/1reeo9TBD21p95SuCOOm6MY8fHtc-pHFRyrjBPd01jtg/pub", '_blank');
+	win.focus();
+}
+
+function expand_content(contentType){
+	var plus = document.getElementsByClassName('fa fa-plus ' + contentType);
+	var minus = document.getElementsByClassName('fa fa-minus ' + contentType);
+	var content = document.getElementsByClassName('content ' + contentType);
+	if (content[0].style.display == 'block'){
+		plus[0].style.display = 'block';
+		minus[0].style.display = 'none';
+		content[0].style.display = 'none';
+	} else {
+		plus[0].style.display = 'none';
+		minus[0].style.display = 'block';
+		content[0].style.display = 'block';
+	}
+
+}
+
+function retract_content(contentType){
+	var plus = document.getElementsByClassName('fa fa-plus ' + contentType);
+	var minus = document.getElementsByClassName('fa fa-minus ' + contentType);
+	var content = document.getElementsByClassName('content ' + contentType);
+	plus[0].style.display = 'block';
+	minus[0].style.display = 'none';
+	content[0].style.display = 'none';
+}
+
